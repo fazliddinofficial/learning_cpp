489bdef638c463c6dd83e71124877cb8717d7366
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2026-04-12 12:32:28 +0500

    Add project files.

---
+++ serious_project/serious_project.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+template<typename T>
+void print(T v) {
+    std::cout << v << '\n';
+}
+
+int main()
+{
+    std::string name = "Ali";
+    class Person {
+    public: 
+        std::string name = "";
+        int age = 0;
+
+        void greet() const{
+            std::cout << "hi " << this->name << '\n';
+        }
+
+        std::string toString() const {
+            return "Person { name: " + this->name + ", age: " + std::to_string(age) + " }";
+        }
+    };
+
+    Person me;
+
+    me.name = "Fazliddin";
+    me.age = 23;
+    me.greet();
+
+    std::cout << me.toString() << '\n';
+
+    return 0;
+}
